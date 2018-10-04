35ec793447c5c792428c09ee1d1e66405f5073d0
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-04 18:23:50 +0200

    continue on error

--- ciscripts/compiletest.sh
+++ ciscripts/compiletest.sh
@@ -3,4 +3,4 @@
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_COVERAGE_TESTING=ON \
 -DBUILD_STEEM_TESTNET=ON -DLOW_MEMORY_NODE=OFF -DCLEAR_VOTES=ON -DSKIP_BY_TX_ID=ON -DCHAINBASE_CHECK_LOCKING=OFF ..
-make -Wno-maybe-uninitialized -j$(nproc) steemd cli_wallet chain_test plugin_test
+make -k -j$(nproc) steemd cli_wallet chain_test plugin_test
