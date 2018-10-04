1386618703a41670c4ec7619b37e911a54d105da
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-04 18:55:31 +0200

    try manually upgrading

--- ciscripts/compiletest.sh
+++ ciscripts/compiletest.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+dpkg -l | grep gcc | awk '{print $2}'
+update-alternatives --set gcc "/usr/bin/gcc-5"
+update-alternatives --set g++ "/usr/bin/g++-5"
+
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_COVERAGE_TESTING=ON \
 -DBUILD_STEEM_TESTNET=ON -DLOW_MEMORY_NODE=OFF -DCLEAR_VOTES=ON -DSKIP_BY_TX_ID=ON -DCHAINBASE_CHECK_LOCKING=OFF ..
