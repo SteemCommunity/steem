13d203367cccea0acf839c8220e4df8ea76469cf
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-04 15:56:24 +0200

    more output for diagnostics

--- ciscripts/installboost.sh
+++ ciscripts/installboost.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 export BOOST_ROOT=$HOME/opt/boost_1_60_0
+ls -al "$BOOST_ROOT"
+ls -al .
 if [ ! -d "boost_1_60_0" ]; then
     URL='http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.bz2/download'
     wget -c "$URL" -O boost_1_60_0.tar.bz2
