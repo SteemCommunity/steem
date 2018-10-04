74af84374b9c8e46cec465d2d405325aebcfde0c
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-04 16:31:48 +0200

    fix no output received

--- ciscripts/installboost.sh
+++ ciscripts/installboost.sh
@@ -6,13 +6,17 @@ if find "$BOOST_ROOT" -mindepth 1 | read; then
 else
     echo "boost not cached!"
     URL='http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.bz2/download'
-    wget -c "$URL" -O boost_1_60_0.tar.bz2
+    wget -nv -c "$URL" -O boost_1_60_0.tar.bz2
+    echo "extracting boost, can take a while..."
+    tar xjf boost_1_60_0.tar.bz2 > /dev/null
+    echo "extracting boost done!"
+    cd boost_1_60_0
     echo "compiling boost, can take a while..."
-    {
-        tar xjf boost_1_60_0.tar.bz2
-        cd boost_1_60_0
-        ./bootstrap.sh "--prefix=$BOOST_ROOT"
-        ./b2 install
-    } > /dev/null
+    ./bootstrap.sh "--prefix=$BOOST_ROOT"
+    echo "extracting boost done!"
+    echo "installing boost, can take a while..."
+    ./b2 install
+    echo "installing boost done!"
+
     echo "boost compilation finished!"
 fi
