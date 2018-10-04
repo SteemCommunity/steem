11a542008a4312cefa2d8bc542648ea2645b5c10
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-04 15:50:09 +0200

    maybe better test for boost already installled

--- ciscripts/installboost.sh
+++ ciscripts/installboost.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 export BOOST_ROOT=$HOME/opt/boost_1_60_0
-if [ ! -d "$BOOST_ROOT" ]; then
+if [ ! -d "boost_1_60_0" ]; then
     URL='http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.bz2/download'
     wget -c "$URL" -O boost_1_60_0.tar.bz2
 {
@@ -9,5 +9,5 @@ if [ ! -d "$BOOST_ROOT" ]; then
     cd boost_1_60_0
     ./bootstrap.sh "--prefix=$BOOST_ROOT"
     ./b2 install
-} &> /dev/null
+} > /dev/null
 fi
