00092f20a5a9f8c1cc8a090d9f41e217ccd41eca
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-04 18:10:05 +0200

    ignore vendor errors

--- ciscripts/compiletest.sh
+++ ciscripts/compiletest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release -Wno-maybe-uninitialized ..
 make -j$(nproc) steemd cli_wallet chain_test plugin_test
