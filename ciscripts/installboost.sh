559b848e85d11d04edc54728b5f7e6125d2daf8c
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-04 16:02:05 +0200

    add check for empty dir

--- ciscripts/installboost.sh
+++ ciscripts/installboost.sh
@@ -3,13 +3,16 @@
 export BOOST_ROOT=$HOME/opt/boost_1_60_0
 ls -al "$BOOST_ROOT"
 ls -al .
-if [ ! -d "boost_1_60_0" ]; then
+if find "$BOOST_ROOT" -mindepth 1 | read; then
+    echo "boost cached!"
+else
+    echo "boost not cached, compiling!"
     URL='http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.bz2/download'
     wget -c "$URL" -O boost_1_60_0.tar.bz2
-#{
+{
     tar xjf boost_1_60_0.tar.bz2
     cd boost_1_60_0
     ./bootstrap.sh "--prefix=$BOOST_ROOT"
     ./b2 install
-#} > /dev/null
+} > /dev/null
 fi
