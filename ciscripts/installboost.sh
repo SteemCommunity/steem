2aad711c80c84172bea4b22b9c46369a88d77beb
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-04 15:53:25 +0200

    enable logging again

--- ciscripts/installboost.sh
+++ ciscripts/installboost.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 export BOOST_ROOT=$HOME/opt/boost_1_60_0
 if [ ! -d "boost_1_60_0" ]; then
     URL='http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.bz2/download'
     wget -c "$URL" -O boost_1_60_0.tar.bz2
-{
+#{
     tar xjf boost_1_60_0.tar.bz2
     cd boost_1_60_0
     ./bootstrap.sh "--prefix=$BOOST_ROOT"
     ./b2 install
-} > /dev/null
+#} > /dev/null
 fi
