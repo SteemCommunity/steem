367059878287933032d589663f7d1a4107f183e8
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-04 22:51:09 +0200

    try only lcov

--- ciscripts/collectcoverage.sh
+++ ciscripts/collectcoverage.sh
@@ -2,11 +2,6 @@
 
 ./build/tests/chain_test
 ./build/tests/plugin_test
-mkdir -p /var/cobertura
-gcovr --object-directory="../" --root=../ --xml-pretty --gcov-exclude=".*tests.*" --gcov-exclude=".*fc.*" \
-      --gcov-exclude=".*app*" --gcov-exclude=".*net*" --gcov-exclude=".*plugins*" --gcov-exclude=".*schema*" \
-      --gcov-exclude=".*time*" --gcov-exclude=".*utilities*" --gcov-exclude=".*wallet*" \
-      --gcov-exclude=".*programs*" --gcov-exclude=".*vendor*" --output="/var/cobertura/coverage.xml"
 
 - lcov --directory . --capture --output-file coverage.info # capture coverage info
 - lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
