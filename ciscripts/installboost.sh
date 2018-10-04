4c846c25ac491e1f2b39a4abcd21341f804b11b9
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-04 16:13:27 +0200

    test for multiple stages

--- ciscripts/installboost.sh
+++ ciscripts/installboost.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 export BOOST_ROOT=$HOME/opt/boost_1_60_0
-ls -al "$BOOST_ROOT"
-ls -al .
 if find "$BOOST_ROOT" -mindepth 1 | read; then
     echo "boost cached!"
 else
-    echo "boost not cached, compiling!"
+    echo "boost not cached!"
     URL='http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.bz2/download'
     wget -c "$URL" -O boost_1_60_0.tar.bz2
-{
-    tar xjf boost_1_60_0.tar.bz2
-    cd boost_1_60_0
-    ./bootstrap.sh "--prefix=$BOOST_ROOT"
-    ./b2 install
-} > /dev/null
+    echo "compiling boost, can take a while..."
+    {
+        tar xjf boost_1_60_0.tar.bz2
+        cd boost_1_60_0
+        ./bootstrap.sh "--prefix=$BOOST_ROOT"
+        ./b2 install
+    } > /dev/null
+    echo "boost compilation finished!"
 fi
