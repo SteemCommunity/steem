1521864aacd1d33c99057e86f4f5d7e043c1df1e
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-04 16:55:42 +0200

    fix package installation

--- ciscripts/installboost.sh
+++ ciscripts/installboost.sh
@@ -8,14 +8,14 @@ else
     URL='http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.bz2/download'
     wget -nv -c "$URL" -O boost_1_60_0.tar.bz2
     echo "extracting boost, can take a while..."
-    tar xjf boost_1_60_0.tar.bz2 > /dev/null
+    tar xjf boost_1_60_0.tar.bz2 | pv -l | wc -l
     echo "extracting boost done!"
     cd boost_1_60_0
     echo "compiling boost, can take a while..."
-    ./bootstrap.sh "--prefix=$BOOST_ROOT" > /dev/null
+    ./bootstrap.sh "--prefix=$BOOST_ROOT" | pv -l | wc -l
     echo "extracting boost done!"
     echo "installing boost, can take a while..."
-    ./b2 install > /dev/null
+    ./b2 install | pv -l | wc -l
     echo "installing boost done!"
 
     echo "boost compilation finished!"
