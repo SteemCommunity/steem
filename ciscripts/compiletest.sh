85158c99457f98c449334e5ee46d652054efa4ca
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-04 19:50:44 +0200

    try xenial

--- ciscripts/compiletest.sh
+++ ciscripts/compiletest.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-gcc --version
+#gcc --version
 
-export CXX=g++-5
-export CC=gcc-5
+#export CXX=g++-5
+#export CC=gcc-5
 
-gcc --version
+#gcc --version
 
-ls -la /usr/bin/ | grep -oP "[\S]*(gcc|g\+\+)(-[a-z]+)*[\s]" | xargs bash -c 'for link in ${@:1}; do sudo ln -s -f "/usr/bin/${link}-${0}" "/usr/bin/${link}"; done' 5
+#ls -la /usr/bin/ | grep -oP "[\S]*(gcc|g\+\+)(-[a-z]+)*[\s]" | xargs bash -c 'for link in ${@:1}; do sudo ln -s -f "/usr/bin/${link}-${0}" "/usr/bin/${link}"; done' 5
 
 gcc --version
 
 dpkg -l | grep gcc | awk '{print $2}'
-update-alternatives --set gcc "/usr/bin/gcc-5"
-update-alternatives --set g++ "/usr/bin/g++-5"
+#update-alternatives --set gcc "/usr/bin/gcc-5"
+#update-alternatives --set g++ "/usr/bin/g++-5"
 
-gcc --version
+#gcc --version
 
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_COVERAGE_TESTING=ON \
