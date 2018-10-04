430b3ed6e50ea31847dc89dd4b26f4b5b0427012
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-04 20:36:55 +0200

    no need to compile wallet

--- ciscripts/compiletest.sh
+++ ciscripts/compiletest.sh
@@ -3,4 +3,4 @@
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_COVERAGE_TESTING=ON \
 -DBUILD_STEEM_TESTNET=ON -DLOW_MEMORY_NODE=OFF -DCLEAR_VOTES=ON -DSKIP_BY_TX_ID=ON -DCHAINBASE_CHECK_LOCKING=OFF ..
-make -j4 steemd cli_wallet chain_test plugin_test
+make -j4 steemd chain_test plugin_test
