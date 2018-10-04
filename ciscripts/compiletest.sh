955e8da5b661d20f600d2bac4a86b4c6d3ee7440
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-04 18:17:30 +0200

    try ignoring errors in make

--- ciscripts/compiletest.sh
+++ ciscripts/compiletest.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -Wno-maybe-uninitialized ..
-make -j$(nproc) steemd cli_wallet chain_test plugin_test
+cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_COVERAGE_TESTING=ON \
+-DBUILD_STEEM_TESTNET=ON -DLOW_MEMORY_NODE=OFF -DCLEAR_VOTES=ON -DSKIP_BY_TX_ID=ON -DCHAINBASE_CHECK_LOCKING=OFF ..
+make -Wno-maybe-uninitialized -j$(nproc) steemd cli_wallet chain_test plugin_test
