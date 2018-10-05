9da0ca2be5bacf472bdcc89d54a5a25e316aeadd
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-05 08:39:23 +0200

    add bigger timeout

--- ciscripts/compiletest.sh
+++ ciscripts/compiletest.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+free -h
+
 mtime_cache **/*.{%{cpp}} -c .mtime_cache/cache.json
 
-free -m
+free -h
 
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_COVERAGE_TESTING=ON \
 -DBUILD_STEEM_TESTNET=ON -DLOW_MEMORY_NODE=OFF -DCLEAR_VOTES=ON -DSKIP_BY_TX_ID=ON -DCHAINBASE_CHECK_LOCKING=OFF ..
+
+free -h
+
 make -j1 steemd chain_test plugin_test
+
+free -h
