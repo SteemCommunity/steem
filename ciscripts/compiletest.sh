751a388ad33c7e2094215dcc97298a89805e6a1a
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-05 08:07:08 +0200

    move mtime to compile step

--- ciscripts/compiletest.sh
+++ ciscripts/compiletest.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mtime_cache **/*.{%{cpp}} -c .mtime_cache/cache.json
+
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_COVERAGE_TESTING=ON \
 -DBUILD_STEEM_TESTNET=ON -DLOW_MEMORY_NODE=OFF -DCLEAR_VOTES=ON -DSKIP_BY_TX_ID=ON -DCHAINBASE_CHECK_LOCKING=OFF ..
