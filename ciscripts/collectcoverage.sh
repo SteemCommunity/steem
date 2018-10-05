6198d900ca9e2ba7f3a700386926f2a9323d9860
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-05 20:22:58 +0200

    clean build files up a bit

--- ciscripts/collectcoverage.sh
+++ ciscripts/collectcoverage.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-./build/tests/chain_test
-./build/tests/plugin_test
-
 echo "collecting coverage now..."
 
-{
 lcov --directory . --capture --output-file coverage.info # capture coverage info
 lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
 lcov --remove coverage.info 'test/*' --output-file coverage.info # filter out test
@@ -14,7 +10,7 @@ lcov --remove coverage.info 'libraries/net/*' --output-file coverage.info
 lcov --remove coverage.info 'libraries/utilities/*' --output-file coverage.info
 lcov --remove coverage.info 'libraries/vendor/*' --output-file coverage.info
 lcov --remove coverage.info 'programs/*' --output-file coverage.info
-} > /dev/null
+
 
 echo "coverage collected to coverage.info"
 ls -al coverage.info
--- ciscripts/compiletest.sh
+++ ciscripts/compiletest.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-free -h
-
 mtime_cache **/*.{%{cpp}} -c .mtime_cache/cache.json
-
-free -h
-
 
 export CC="ccache gcc"
 export CXX="ccache g++"
@@ -13,16 +8,11 @@ export CCACHE_DIR=.ccache
 # Set max cache size so we don't carry old objects for too long
 ccache -M 250M
 
-export CXXFLAGS="$(CXXFLAGS) --param ggc-min-expand=0 --param ggc-min-heapsize=8192"
-
 ccache -z
 
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_COVERAGE_TESTING=ON \
 -DBUILD_STEEM_TESTNET=ON -DLOW_MEMORY_NODE=OFF -DCLEAR_VOTES=ON -DSKIP_BY_TX_ID=ON -DCHAINBASE_CHECK_LOCKING=OFF ..
 
-free -h
-
 make -j1 steemd chain_test plugin_test test_fixed_string
 
-free -h
