96722841906e512482ace6b7550ab2d0f8156ce4
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-05 09:07:21 +0200

    remove coverage printout

--- ciscripts/collectcoverage.sh
+++ ciscripts/collectcoverage.sh
@@ -5,6 +5,5 @@
 
 lcov --directory . --capture --output-file coverage.info # capture coverage info
 lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
-lcov --list coverage.info #debug info
-# Uploading report to CodeCov
+
 wget -O - https://codecov.io/bash | bash || echo "Codecov did not collect coverage reports"
