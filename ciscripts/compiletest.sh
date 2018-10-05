2b68562f9768d7c1b1540d7292bfb69152b3bbb1
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-05 11:13:28 +0200

    try again preserving cache

--- ciscripts/compiletest.sh
+++ ciscripts/compiletest.sh
@@ -9,7 +9,7 @@ free -h
 
 export CC="ccache gcc"
 export CXX="ccache g++"
-export CCACHE_DIR=~/.ccache
+export CCACHE_DIR=.ccache
 # Set max cache size so we don't carry old objects for too long
 ccache -M 400M
 
@@ -23,6 +23,6 @@ cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_COVERAGE_TESTING=ON \
 
 free -h
 
-make -j2 steemd chain_test plugin_test test_fixed_string
+make -j1 steemd chain_test plugin_test test_fixed_string
 
 free -h
