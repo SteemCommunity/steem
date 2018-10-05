ffc19a170853639339602eb6338acca5596daa2e
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-05 08:16:19 +0200

    use amount of cores available

--- ciscripts/compiletest.sh
+++ ciscripts/compiletest.sh
@@ -5,4 +5,4 @@ mtime_cache **/*.{%{cpp}} -c .mtime_cache/cache.json
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_COVERAGE_TESTING=ON \
 -DBUILD_STEEM_TESTNET=ON -DLOW_MEMORY_NODE=OFF -DCLEAR_VOTES=ON -DSKIP_BY_TX_ID=ON -DCHAINBASE_CHECK_LOCKING=OFF ..
-make -j4 steemd chain_test plugin_test
+make -j$(nproc) steemd chain_test plugin_test
