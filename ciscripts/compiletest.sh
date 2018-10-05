eb515b4ca3ae50b7f0901847414aa04023356cb1
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-05 08:28:02 +0200

    really only use one core?

--- ciscripts/compiletest.sh
+++ ciscripts/compiletest.sh
@@ -7,4 +7,4 @@ free -m
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_COVERAGE_TESTING=ON \
 -DBUILD_STEEM_TESTNET=ON -DLOW_MEMORY_NODE=OFF -DCLEAR_VOTES=ON -DSKIP_BY_TX_ID=ON -DCHAINBASE_CHECK_LOCKING=OFF ..
-make -j2 steemd chain_test plugin_test
+make -j1 steemd chain_test plugin_test
