1482c94442e57afa9800c80e74fe93e17447cb4f
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-05 09:16:27 +0200

    try out ccache

--- ciscripts/compiletest.sh
+++ ciscripts/compiletest.sh
@@ -6,6 +6,15 @@ mtime_cache **/*.{%{cpp}} -c .mtime_cache/cache.json
 
 free -h
 
+
+export CC="ccache gcc"
+export CXX="ccache g++"
+export CCACHE_DIR=~/.ccache
+# Set max cache size so we don't carry old objects for too long
+ccache -M 400M
+
+ccache -z
+
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_COVERAGE_TESTING=ON \
 -DBUILD_STEEM_TESTNET=ON -DLOW_MEMORY_NODE=OFF -DCLEAR_VOTES=ON -DSKIP_BY_TX_ID=ON -DCHAINBASE_CHECK_LOCKING=OFF ..
