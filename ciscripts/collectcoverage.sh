6c8ff53488bdd1341dc9bff71a79aa545053644a
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-05 08:00:23 +0200

    try build

--- ciscripts/collectcoverage.sh
+++ ciscripts/collectcoverage.sh
@@ -7,4 +7,4 @@ lcov --directory . --capture --output-file coverage.info # capture coverage info
 lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
 lcov --list coverage.info #debug info
 # Uploading report to CodeCov
-bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
+curl -s https://codecov.io/bash || echo "Codecov did not collect coverage reports"
