c1077b018b801c1fe7523bc45585af593eec975f
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-05 09:01:36 +0200

    try debug build to have lees optimization

--- ciscripts/compiletest.sh
+++ ciscripts/compiletest.sh
@@ -7,7 +7,7 @@ mtime_cache **/*.{%{cpp}} -c .mtime_cache/cache.json
 free -h
 
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_COVERAGE_TESTING=ON \
+cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_COVERAGE_TESTING=ON \
 -DBUILD_STEEM_TESTNET=ON -DLOW_MEMORY_NODE=OFF -DCLEAR_VOTES=ON -DSKIP_BY_TX_ID=ON -DCHAINBASE_CHECK_LOCKING=OFF ..
 
 free -h
