ea126dffc59ba3694f9d87ba3247051927e25bb3
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-05 14:39:43 +0200

    improved coverage generation

--- ciscripts/collectcoverage.sh
+++ ciscripts/collectcoverage.sh
@@ -3,6 +3,9 @@
 ./build/tests/chain_test
 ./build/tests/plugin_test
 
+echo "collecting coverage now..."
+
+{
 lcov --directory . --capture --output-file coverage.info # capture coverage info
 lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
 lcov --remove coverage.info 'test/*' --output-file coverage.info # filter out test
@@ -11,3 +14,7 @@ lcov --remove coverage.info 'libraries/net/*' --output-file coverage.info
 lcov --remove coverage.info 'libraries/utilities/*' --output-file coverage.info
 lcov --remove coverage.info 'libraries/vendor/*' --output-file coverage.info
 lcov --remove coverage.info 'programs/*' --output-file coverage.info
+} > /dev/null
+
+echo "coverage collected to coverage.info"
+ls -al coverage.info
