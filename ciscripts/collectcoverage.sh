6dc3e54876cada9878f68a23526e3d762f57e73c
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-10-05 21:23:59 +0200

    don't delete all coverage

--- ciscripts/collectcoverage.sh
+++ ciscripts/collectcoverage.sh
@@ -3,7 +3,11 @@
 echo "collecting coverage now..."
 
 lcov --directory . --capture --output-file coverage.info # capture coverage info
-lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
+lcov --remove coverage.info '/usr/bin/*' --output-file coverage.info # filter out system
+lcov --remove coverage.info '/usr/lib/*' --output-file coverage.info # filter out system
+lcov --remove coverage.info '/usr/include/*' --output-file coverage.info # filter out system
+lcov --remove coverage.info '/usr/sbin/*' --output-file coverage.info # filter out system
+lcov --remove coverage.info '/usr/share/*' --output-file coverage.info # filter out system
 lcov --remove coverage.info 'test/*' --output-file coverage.info # filter out test
 lcov --remove coverage.info 'libraries/fc/*' --output-file coverage.info
 lcov --remove coverage.info 'libraries/net/*' --output-file coverage.info
